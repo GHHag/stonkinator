96ebda43a3c9f9ea9e3c567167c475bc1917e3ba
GHHag <gustav.hagenblad@gmail.com>
2023-04-23 22:36:07 +0200

    create logs dir if not existing

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 cp .env ./tet_py_packages/tet_trading_systems/tet_trading_systems/trading_system_development/trading_systems/env.py 
 cp .env ./tet_py_packages/securities_db_py_dal/securities_db_py_dal/env.py
 
-#if [[ ! -d ./tet_py_packages/securities_db_py_dal/logs ]]; then
-	#mkdir ./tet_py_packages/securities_db_py_dal/logs
-#fi
+if [[ ! -d ./tet_py_packages/securities_db_py_dal/logs ]]; then
+	mkdir ./tet_py_packages/securities_db_py_dal/logs
+fi
 
 docker compose -f ./docker-compose.yml up -d
