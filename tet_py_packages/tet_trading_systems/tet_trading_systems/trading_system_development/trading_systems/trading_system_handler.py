8e0e2bea3c12c63393f274a7ea8cfb8f25ef6915
GHHag <gustav.hagenblad@gmail.com>
2023-09-22 22:39:24 +0200

    run ts moved to ts handler

--- tet_py_packages/tet_trading_systems/tet_trading_systems/trading_system_development/trading_systems/trading_system_handler.py
+++ tet_py_packages/tet_trading_systems/tet_trading_systems/trading_system_development/trading_systems/trading_system_handler.py
@@ -7,11 +7,10 @@ import json
 
 from TETrading.data.metadata.trading_system_attributes import TradingSystemAttributes
 from TETrading.data.metadata.market_state_enum import MarketState
+from TETrading.trading_system.trading_system import TradingSystem
 
 from tet_trading_systems.trading_system_development.trading_systems.trading_system_properties.trading_system_properties \
     import TradingSystemProperties
-from tet_trading_systems.trading_system_development.trading_systems.trading_system_properties.ml_trading_system_properties \
-    import MlTradingSystemProperties
 
 from tet_trading_systems.trading_system_management.position_sizer.position_sizer import IPositionSizer
 
@@ -23,6 +22,55 @@ from tet_doc_db.tet_mongo_db.systems_mongo_db import TetSystemsMongoDb
 from tet_doc_db.tet_mongo_db.portfolio_mongo_db import TetPortfolioMongoDb
 from tet_doc_db.time_series_mongo_db.time_series_mongo_db import TimeSeriesMongoDb
 from tet_doc_db.instruments_mongo_db.instruments_mongo_db import InstrumentsMongoDb
+
+
+def run_trading_system(
+    data_dict, system_name, entry_func, exit_func,
+    entry_args, exit_args, *args, 
+    capital=10000, capital_fraction=1.0, avg_yearly_periods=251,  
+    market_state_null_default=False, run_monte_carlo_sims=False, num_of_sims=2500,
+    print_dataframe=False, plot_fig=False, plot_positions=False, write_to_file_path=None,
+    save_summary_plot_to_path=False, system_analysis_to_csv_path=None,
+    plot_returns_distribution=False,
+    systems_db: ITetSystemsDocumentDatabase=None, 
+    client_db: ITetSystemsDocumentDatabase=None, 
+    print_data=False,
+    run_from_latest_exit=False,
+    insert_into_db=False,
+    pos_list_slice_years_est=2,
+    save_best_estimate_trades_path=None,
+    **kwargs
+):
+    ts = TradingSystem(system_name, data_dict, entry_func, exit_func, systems_db, client_db)
+    ts(
+        capital=capital,
+        capital_fraction=capital_fraction,
+        avg_yearly_periods=avg_yearly_periods,
+        market_state_null_default=market_state_null_default,
+        plot_performance_summary=plot_fig,
+        save_summary_plot_to_path=save_summary_plot_to_path,
+        system_analysis_to_csv_path=system_analysis_to_csv_path, 
+        plot_returns_distribution=plot_returns_distribution,
+        save_returns_distribution_plot_to_path=None, 
+        run_monte_carlo_sims=run_monte_carlo_sims,
+        num_of_monte_carlo_sims=num_of_sims,
+        monte_carlo_data_amount=0.65,
+        plot_monte_carlo=plot_fig,
+        print_monte_carlo_df=print_dataframe,
+        monte_carlo_analysis_to_csv_path=None, 
+        print_data=print_data,
+        commission_pct_cost=0.0025,
+        entry_args=entry_args,
+        exit_args=exit_args,
+        fixed_position_size=True,
+        generate_signals=True,
+        plot_positions=plot_positions,
+        save_position_figs_path=None,
+        write_signals_to_file_path=write_to_file_path,
+        run_from_latest_exit=run_from_latest_exit,
+        insert_data_to_db_bool=insert_into_db,
+        pos_list_slice_years_est=pos_list_slice_years_est
+    )
 
 
 def handle_trading_system(
@@ -37,15 +85,16 @@ def handle_trading_system(
         # make sure exit dates are available to get for the given trading system
         latest_position_dts = json.loads(
             client_db.get_latest_position_dts(
-                # make symbols_list a member of TradingSystemProperties instead
-                system_props.system_name, list(system_props.preprocess_data_args)[0]
+                system_props.system_name, system_props.system_instruments_list
             )
         )
 
     data, pred_features_data = system_props.preprocess_data_function(
+        system_props.system_instruments_list,
         *system_props.preprocess_data_args, start_dt, end_dt, 
         latest_position_dts=latest_position_dts if from_latest_exit else from_latest_exit
     )
+
     #if time_series_db:
     #    time_series_db.insert_pandas_time_series_data(data)
 
@@ -56,11 +105,13 @@ def handle_trading_system(
         *system_props.position_sizer_args
     )
 
-    for _ in range(system_props.required_runs):
+    for i in range(system_props.required_runs):
         system_state_handler(
             *system_props.system_state_handler_call_args, pred_features_data,
-            plot_fig=plot_fig, client_db=client_db, insert_into_db=insert_into_db,
+            plot_fig=plot_fig, client_db=client_db, 
+            insert_into_db=True if (i + 1) == system_props.required_runs and insert_into_db else False,
             **system_props.system_state_handler_call_kwargs,
+            run_from_latest_exit=from_latest_exit,
             **system_position_sizer.position_sizer_data_dict
         )
         market_states_data: List[Dict] = json.loads(
@@ -96,9 +147,19 @@ def handle_ext_pos_sizer_trading_system(
     time_series_db: ITimeSeriesDocumentDatabase=None,
     insert_into_db=False, plot_fig=False 
 ):
+    if from_latest_exit:
+        latest_position_dts = json.loads(
+            client_db.get_latest_position_dts(
+                system_props.system_name, system_props.system_instruments_list
+            )
+        )
+
     data, pred_features_data = system_props.preprocess_data_function(
-        *system_props.preprocess_data_args, start_dt, end_dt
+        system_props.system_instruments_list,
+        *system_props.preprocess_data_args, start_dt, end_dt,
+        latest_position_dts=latest_position_dts if from_latest_exit else from_latest_exit
     )
+
     #if time_series_db:
     #    time_series_db.insert_pandas_time_series_data(data)
 
@@ -109,11 +170,13 @@ def handle_ext_pos_sizer_trading_system(
         *system_props.position_sizer_args
     )
 
-    for _ in range(system_props.required_runs):
+    for i in range(system_props.required_runs):
         system_state_handler(
             *system_props.system_state_handler_call_args, pred_features_data,
-            plot_fig=plot_fig, client_db=client_db, insert_into_db=insert_into_db,
+            plot_fig=plot_fig, client_db=client_db,
+            insert_into_db=True if (i + 1) == system_props.required_runs and insert_into_db else False,
             **system_props.system_state_handler_call_kwargs,
+            run_from_latest_exit=from_latest_exit,
             **system_position_sizer.position_sizer_data_dict
         )
         position_list, num_of_periods = systems_db.get_position_list(
@@ -131,7 +194,7 @@ def handle_ext_pos_sizer_trading_system(
  
 
 def handle_ml_trading_system(
-    system_props: MlTradingSystemProperties, start_dt, end_dt, 
+    system_props: TradingSystemProperties, start_dt, end_dt, 
     from_latest_exit: bool,
     systems_db: ITetSystemsDocumentDatabase, 
     client_db: ITetSignalsDocumentDatabase, 
@@ -139,8 +202,10 @@ def handle_ml_trading_system(
     insert_into_db=False, plot_fig=False 
 ):
     data, pred_features_data = system_props.preprocess_data_function(
+        system_props.system_instruments_list,
         *system_props.preprocess_data_args, start_dt, end_dt
     )
+
     #if time_series_db:
     #   time_series_db.insert_pandas_time_series_data(data)
 
@@ -235,14 +300,16 @@ if __name__ == '__main__':
     #start_dt = dt.datetime(1999, 1, 1)
     #end_dt = dt.datetime(2011, 1, 1)
     start_dt = dt.datetime(2015, 9, 16)
-    #start_dt = dt.datetime(2023, 1, 21)
     #end_dt = dt.datetime.now()
-    end_dt = dt.datetime(2023, 2, 27)
+    #end_dt = dt.datetime(2023, 2, 24)
+    end_dt = dt.datetime(2023, 3, 1)
 
     systems_props_list: List[TradingSystemProperties] = []
 
     for trading_system in trading_system_modules:
+        #if trading_system == 'ml_trading_system_example':
         if trading_system != 'mean_reversion_stocks':
+        #if trading_system != 'trading_system_example':
             continue
         systems_props_list.append(
             __globals[trading_system].get_props(
@@ -252,7 +319,7 @@ if __name__ == '__main__':
         )
 
     for system_props in systems_props_list:
-        # implementera protocol för system_handler_function?
+        # TODO: implement protocol for system handler functions
         system_props.system_handler_function(
             system_props, start_dt, end_dt, 
             from_latest_exit,
