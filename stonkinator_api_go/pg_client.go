084317e8fd2d8f5cc0b73327dafc3bdb7c95f4a7
GHHag <gustav.hagenblad@gmail.com>
2023-10-22 14:06:18 +0200

    pg client

--- stonkinator_api_go/pg_client.go
+++ stonkinator_api_go/pg_client.go
@@ -8,27 +8,27 @@ import (
 	"fmt"
 )
 
-var PgPool pgxpool.Pool
-var Context = context.Background()
+var PgPool *pgxpool.Pool
 
-func PgClient() (*pgxpool.Pool, context.Context, error) {
+func init() {
 	connectionString := fmt.Sprintf(
 		"postgresql://%s:%s@%s:%s/%s",
 		os.Getenv("PG_DB_USER"),
 		os.Getenv("PG_DB_PASSWORD"),
 		os.Getenv("PG_DB_SERVICE"),
-		os.Getenv("PG_DB_PORT"),
+		// os.Getenv("PG_DB_PORT"),
+		os.Getenv("PG_DB_PORT_EXP"),
 		os.Getenv("PG_DB_NAME"),
 	)
 
-	context := context.Background()
-
-	PgPool, err := pgxpool.New(context, connectionString)
-
+	var err error
+	PgPool, err = pgxpool.New(context.Background(), connectionString)
 	if err != nil {
 		log.Fatal("Error connecting to the database: ", err)
-		return nil, nil, err
+		panic(err)
 	}
 
-	return PgPool, context, nil
+	if PgPool == nil {
+		panic(err)
+	}
 }
