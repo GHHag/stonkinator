5e5ef8e3a0bf40672442b44c03c756c99cdd6030
GHHag <gustav.hagenblad@gmail.com>
2024-01-13 01:31:19 +0100

    ts state handler impl

--- tet_py_packages/tet_trading_systems/tet_trading_systems/trading_system_state_handler/trading_system_state_handler.py
+++ tet_py_packages/tet_trading_systems/tet_trading_systems/trading_system_state_handler/trading_system_state_handler.py
@@ -33,7 +33,7 @@ class TradingSystemStateHandler:
             systems_db, client_db
         )
         self.__systems_db: ITetSystemsDocumentDatabase = systems_db
-        self.__client_db: ITetSystemsDocumentDatabase = client_db
+        # self.__client_db: ITetSystemsDocumentDatabase = client_db
         self.__start_dt: dt.datetime = start_dt
         self.__end_dt: dt.datetime = end_dt
         self.__preprocess_data(self.__start_dt, self.__end_dt)
@@ -53,81 +53,23 @@ class TradingSystemStateHandler:
         # self.__data = nets.DataHandler(new_data)
         pass
 
-    def _run_trading_system(self, *args, **kwargs):
-        print(args)
-        print(*args)
-
-        # make some date check on given date against last date of self.__data
-
-        self.__trading_system(self.__data, *args, **kwargs)
-
-    def _handle_trading_system(
-        self, *args, from_latest_exit: bool=False,
-        insert_into_db=False, plot_fig=False, **kwargs
-    ):
-        # system_state_handler prop has been replaces by this class
-        # system_state_handler = self.__ts_properties.system_state_handler(
-        #     *self.__ts_properties.system_state_handler_args, self.__systems_db, data
-        # )
-        system_position_sizer: IPositionSizer = self.__ts_properties.position_sizer(
-            *self.__ts_properties.position_sizer_args
-        )
-
-        for i in range(self.__ts_properties.required_runs):
-            self._run_trading_system(
-                *self.__ts_properties.system_state_handler_call_args, pred_features_data,
-                plot_fig=plot_fig, client_db=self.__client_db, 
-                insert_into_db=True if (i + 1) == self.__ts_properties.required_runs and insert_into_db else False,
-                **self.__ts_properties.system_state_handler_call_kwargs,
-                run_from_latest_exit=from_latest_exit,
-                **system_position_sizer.position_sizer_data_dict
-            )
-            market_states_data: list[dict] = json.loads(
-                self.__systems_db.get_market_state_data(
-                    self.__ts_properties.system_name, MarketState.ENTRY.value
-                )
-            )
-
-            for data_dict in market_states_data:
-                position_list, num_of_periods = self.__systems_db.get_single_symbol_position_list(
-                    self.__ts_properties.system_name, data_dict[TradingSystemAttributes.SYMBOL],
-                    return_num_of_periods=True
-                )
-                system_position_sizer(
-                    position_list, num_of_periods,
-                    *self.__ts_properties.position_sizer_call_args,
-                    symbol=data_dict[TradingSystemAttributes.SYMBOL], 
-                    **self.__ts_properties.position_sizer_call_kwargs,
-                    **system_position_sizer.position_sizer_data_dict
-                )
-
-        pos_sizer_data_dict = system_position_sizer.get_position_sizer_data_dict()
-        self.__systems_db.insert_market_state_data(
-            self.__ts_properties.system_name, json.dumps(pos_sizer_data_dict)
-        )
-
-    # def __call__(self, date: dt.datetime, *args, **kwargs):
-    def __call__(
-        self, 
-        *args, 
+    def _run_trading_system(
+        self,
         capital=10000, capital_fraction=1.0, avg_yearly_periods=251,  
         market_state_null_default=False, run_monte_carlo_sims=False, num_of_sims=2500,
         print_dataframe=False, plot_fig=False, plot_positions=False, write_to_file_path=None,
         save_summary_plot_to_path=False, system_analysis_to_csv_path=None,
         plot_returns_distribution=False,
-        print_data=True,
+        print_data=False,
         run_from_latest_exit=False,
         insert_into_db=False,
         pos_list_slice_years_est=2,
         **kwargs
     ):
-        # make some date check on given date against last date of self.__data
-        # self._run_trading_system(*args, **kwargs)
-
-        print(args)
-        print(*args)
-        self._handle_trading_system(
-            *args,
+        print(capital_fraction)
+        input('capital fraction _run_trading_system')
+        self.__trading_system(
+            self.__data,
             capital=capital,
             capital_fraction=capital_fraction,
             avg_yearly_periods=avg_yearly_periods,
@@ -154,6 +96,59 @@ class TradingSystemStateHandler:
             write_signals_to_file_path=write_to_file_path,
             run_from_latest_exit=run_from_latest_exit,
             insert_data_to_db_bool=insert_into_db,
-            pos_list_slice_years_est=pos_list_slice_years_est,
+            pos_list_slice_years_est=pos_list_slice_years_est
+        )
+
+    def _handle_trading_system(
+        self, time_series_db=None, insert_into_db=False, plot_fig=False, **kwargs
+    ):
+        system_position_sizer: IPositionSizer = self.__ts_properties.position_sizer(
+            *self.__ts_properties.position_sizer_args
+        )
+
+        for i in range(self.__ts_properties.required_runs):
+            insert_data = True if (i + 1) == self.__ts_properties.required_runs and insert_into_db else False
+            print(system_position_sizer.position_sizer_data_dict)
+            input('pos sizer data dict')
+            self._run_trading_system(
+                plot_fig=plot_fig, 
+                insert_into_db=insert_data,
+                **system_position_sizer.position_sizer_data_dict
+            )
+            market_states_data: list[dict] = json.loads(
+                self.__systems_db.get_market_state_data(
+                    self.__ts_properties.system_name, MarketState.ENTRY.value
+                )
+            )
+
+            for data_dict in market_states_data:
+                position_list, num_of_periods = self.__systems_db.get_single_symbol_position_list(
+                    self.__system_props.system_name, data_dict[TradingSystemAttributes.SYMBOL],
+                    return_num_of_periods=True
+                )
+                system_position_sizer(
+                    position_list, num_of_periods,
+                    *self.__ts_properties.position_sizer_call_args,
+                    symbol=data_dict[TradingSystemAttributes.SYMBOL], 
+                    **self.__ts_properties.position_sizer_call_kwargs,
+                    **system_position_sizer.position_sizer_data_dict
+                )
+
+        pos_sizer_data_dict = system_position_sizer.get_position_sizer_data_dict()
+        self.__systems_db.insert_market_state_data(
+            self.__ts_properties.system_name, json.dumps(pos_sizer_data_dict)
+        )
+
+    def __call__(
+        self, date: dt.datetime,
+        time_series_db=None, insert_into_db=False, plot_fig=False, **kwargs
+    ):
+        # make some date check on given date against last date of self.__data
+        # call reprocess_data if new data is available
+        # self.reprocess_data(date)
+
+        self._handle_trading_system(
+            time_series_db=time_series_db, 
+            insert_into_db=insert_into_db, plot_fig=plot_fig,
             **kwargs
         )
