3eff48912b07f097252c36580ffccd61e8e3b208
GHHag <gustav.hagenblad@gmail.com>
2024-02-16 23:26:17 +0100

    alt TradingSession class

--- tet_py_packages/tetrading/TETrading/trading_system/trading_session.py
+++ tet_py_packages/tetrading/TETrading/trading_system/trading_session.py
@@ -9,6 +9,257 @@ from TETrading.signal_events.signal_handler import SignalHandler
 from TETrading.plots.candlestick_plots import candlestick_plot
 
 
+class TradingSessionAlt:
+    """
+    A class that represents a trading session. By using the given
+    dataframe together with the given entry and exit logic a
+    TradingSession instance generates Position objects.
+
+    Parameters
+    ----------
+    entry_logic_function: 'function'
+        A function with logic for when to enter a market.
+    exit_logic_function: 'function'
+        A function with logic for when to exit a market.
+    dataframe: 'Pandas.DataFrame'
+        Data in the form of a Pandas DataFrame.
+    signal_handler: Keyword arg 'None/SignalHandler'
+        An instance of the SignalHandler class. Handles
+        data from generated events/signals.
+        Default value=None
+    symbol: Keyword arg 'str'
+        The ticker/symbol of the instrument to be traded
+        in the current trading session. Default value=''
+    """
+
+    def __init__(
+        self, entry_logic_function, exit_logic_function,
+        signal_handler: SignalHandler, symbol=''
+    ):
+        self.__entry_logic_function = entry_logic_function
+        self.__exit_logic_function = exit_logic_function
+        self.__signal_handler = signal_handler
+        self.__symbol = symbol
+        self.__market_state_column = TradingSystemAttributes.MARKET_STATE
+
+    def __call__(
+        self, *args, entry_args=None, exit_args=None, 
+        max_req_periods_feature=TradingSystemAttributes.REQ_PERIOD_ITERS, 
+        dataframe: pd.DataFrame, position: Position, current_index,
+        datetime_col_name='date',
+        close_price_col_name='close', open_price_col_name='open',
+        fixed_position_size=True, capital=10000, commission_pct_cost=0.0,
+        market_state_null_default=False,
+        generate_signals=False, plot_positions=False, 
+        save_position_figs_path=None,
+        print_data=False, **kwargs
+    ):
+        """
+        Generates positions using the __entry_logic_function and 
+        __exit_logic_function members. If the given value for 
+        generate_signals is True, signals will be generated from 
+        the most recent data.
+
+        Parameters
+        ----------
+        :param args:
+            'tuple' : A tuple with arguments.
+        :param entry_args:
+            Keyword arg 'None/dict' : Key-value pairs with parameters used 
+            with the entry logic. Default value=None
+        :param exit_args:
+            Keyword arg 'None/dict' : Key-value pairs with parameters used 
+            with the exit logic. Default value=None
+        :param max_req_periods_feature:
+            Keyword arg 'str' : A key contained in the entry_args dict 
+            that should have the value of the number of periods required 
+            for all features to be calculated before being able to 
+            generate signals from the data. Default value='req_period_iters'
+        :param datetime_col_name:
+            Keyword arg 'str' : The column of the objects __dataframe that
+            contains time and date data. Default value='date'
+        :param close_price_col_name:
+            Keyword arg 'str' : The column of the objects __dataframe that
+            contains values for close prices. Default value='close'
+        :param open_price_col_name:
+            Keyword arg 'str' : The column of the objects __dataframe that
+            contains values for open prices. Default value='open'
+        :param fixed_position_size:
+            Keyword arg 'bool' : True/False decides whether the capital
+            used for positions generated should be at a fixed amount or not.
+            Default value=True
+        :param capital:
+            Keyword arg 'int/float' : The capital given to purchase assets
+            with. Default value=10000
+        :param commission_pct_cost:
+            Keyword arg 'float' : The transaction cost given as a percentage
+            (a float from 0.0 to 1.0) of the total transaction.
+            Default value=0.0
+        :param market_state_null_default:
+            Keyword arg 'bool' : True/False decides whether the market_state
+            property should be assigned a null value by default or not.
+            Default value=False
+        :param generate_signals:
+            Keyword arg 'bool' : True/False decides whether or not market
+            events/signals should be generated from the most recent data.
+            Default value=False
+        :param plot_positions:
+            Keyword arg 'bool' : True/False decides whether or not a plot of
+            a candlestick chart visualizing the points of buying and selling a
+            position should be displayed. Default value=False
+        :param save_position_figs_path:
+            Keyword arg 'None/str' : Provide a file path as a str to save a
+            candlestick chart visualizing the points of buying and selling a
+            position. Default value=None
+        :param print_data:
+            Keyword arg 'bool' : True/False decides whether to print data
+            of positions and signals or not. Default value=False
+        :param kwargs:
+            'dict' : A dictionary with keyword arguments.
+        """
+
+        # implement these variables as fields in Position, to be persisted
+        # in database object too?
+        # trailing_exit, trailing_exit_price = False, None 
+
+        if position.active_position is True:
+            position.update(
+                Decimal(dataframe[close_price_col_name].iloc[-1])
+            )
+
+            if position.exit_signal_dt:
+            # if position.exit_signal_given:
+                capital = position.exit_market(
+                    dataframe[open_price_col_name].iloc[-1], 
+                    dataframe[datetime_col_name].iloc[-1]
+                )
+                position.set_price_data_json(
+                    dataframe.iloc[-len(position.returns_list)-15:]
+                    [[open_price_col_name, 'high', 'low', close_price_col_name, 'volume', datetime_col_name]].to_json()
+                )
+                if print_data:
+                    position.print_position_stats()
+                    print(
+                        f'Exit index {current_index}: '
+                        f'{format(dataframe[open_price_col_name].iloc[-1], ".3f")}, '
+                        f'{dataframe[datetime_col_name].iloc[-1]}\n'
+                        f'Realised return: {position.position_return}'
+                    )
+                # if plot_positions:
+                #     if save_position_figs_path is not None:
+                #         position_figs_path = save_position_figs_path + (
+                #             fr'\{dataframe.iloc[(current_index - len(position.returns_list))].Date.strftime("%Y-%m-%d")}.jpg'
+                #         )
+                #     else:
+                #         position_figs_path = save_position_figs_path
+                #     candlestick_plot(
+                #         dataframe.iloc[
+                #             (current_index-len(position.returns_list)-20):(current_index+15)
+                #         ],
+                #         position.entry_dt, position.entry_price, 
+                #         dataframe[datetime_col_name].iloc[current_index], 
+                #         dataframe[open_price_col_name].iloc[current_index], 
+                #         save_fig_to_path=position_figs_path
+                #     )
+                return position
+
+        if not position.active_position and not position.entry_dt:
+            position.enter_market(
+                dataframe[open_price_col_name].iloc[-1], direction, 
+                dataframe[datetime_col_name].iloc[-1]
+            )
+            if print_data:
+                print(
+                    f'\nEntry index {len(dataframe)}: '
+                    f'{format(dataframe[open_price_col_name].iloc[-1], ".3f")}, '
+                    f'{dataframe[datetime_col_name].iloc[-1]}'
+                )
+
+        # Handle the trading sessions current market state/events/signals.
+        if market_state_null_default and generate_signals:
+            self.__signal_handler.handle_entry_signal(
+                self.__symbol, {
+                    TradingSystemAttributes.SIGNAL_DT: dataframe[datetime_col_name].iloc[-1],
+                    self.__market_state_column: 'null'
+                }
+            )
+            return
+        if position.active_position and generate_signals:
+            position.update(Decimal(dataframe[close_price_col_name].iloc[-1]))
+            if print_data:
+                position.print_position_status()
+            self.__signal_handler.handle_active_position(
+                self.__symbol, {
+                    TradingSystemAttributes.SIGNAL_INDEX: len(dataframe), 
+                    TradingSystemAttributes.SIGNAL_DT: 
+                        dataframe[datetime_col_name].iloc[-1], 
+                    TradingSystemAttributes.SYMBOL: self.__symbol, 
+                    TradingSystemAttributes.DIRECTION: direction,
+                    TradingSystemAttributes.PERIODS_IN_POSITION: 
+                        len(position.returns_list), 
+                    TradingSystemAttributes.UNREALISED_RETURN: position.unrealised_return,
+                    self.__market_state_column: MarketState.ACTIVE.value
+                }
+            )
+            exit_condition, trailing_exit_price, trailing_exit = self.__exit_logic_function(
+                dataframe, trailing_exit, trailing_exit_price, 
+                position.entry_price, len(position.returns_list), 
+                position.unrealised_return, exit_args=exit_args
+            )
+            if exit_condition:
+                # set some property on Position object to indicate exit signal is given
+                position.exit_signal_dt = dataframe[datetime_col_name].iloc[-1]
+                # position.exit_signal_given = True
+                self.__signal_handler.handle_exit_signal(
+                    self.__symbol, {
+                        TradingSystemAttributes.SIGNAL_INDEX: len(dataframe), 
+                        TradingSystemAttributes.SIGNAL_DT: 
+                            dataframe[datetime_col_name].iloc[-1], 
+                        TradingSystemAttributes.SYMBOL: self.__symbol, 
+                        TradingSystemAttributes.DIRECTION: direction,
+                        TradingSystemAttributes.PERIODS_IN_POSITION: 
+                            len(position.returns_list),
+                        TradingSystemAttributes.UNREALISED_RETURN: 
+                            position.unrealised_return,
+                        self.__market_state_column: MarketState.EXIT.value
+                    }
+                )
+                if print_data: 
+                    print(f'\nExit signal, exit next open\nIndex {len(dataframe)}')
+        elif not position.active_position and generate_signals:
+            entry_signal, direction = self.__entry_logic_function(
+                dataframe, entry_args=entry_args
+            )
+            if entry_signal:
+                position = Position(
+                    capital, fixed_position_size=fixed_position_size, 
+                    commission_pct_cost=commission_pct_cost
+                )
+                self.__signal_handler.handle_entry_signal(
+                    self.__symbol, {
+                        TradingSystemAttributes.SIGNAL_INDEX: len(dataframe), 
+                        TradingSystemAttributes.SIGNAL_DT: 
+                            dataframe[datetime_col_name].iloc[-1], 
+                        TradingSystemAttributes.SYMBOL: self.__symbol,
+                        TradingSystemAttributes.DIRECTION: direction,
+                        self.__market_state_column: MarketState.ENTRY.value
+                    }
+                )
+                if print_data: 
+                    print(f'\nEntry signal, buy next open\nIndex {len(dataframe)}')
+                return position
+
+# from decimal import Decimal
+
+# import pandas as pd
+
+# from TETrading.data.metadata.market_state_enum import MarketState
+# from TETrading.data.metadata.trading_system_attributes import TradingSystemAttributes
+# from TETrading.position.position import Position
+# from TETrading.signal_events.signal_handler import SignalHandler
+# from TETrading.plots.candlestick_plots import candlestick_plot
+
+
 class TradingSession:
     """
     A class that represents a trading session. By using the given
