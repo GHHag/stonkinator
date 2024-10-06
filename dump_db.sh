6ad3cf08f6673b5a30ff37e06361371b8463d795
GHHag <gustav.hagenblad@gmail.com>
2024-10-06 12:23:04 +0200

    db dump

--- dump_db.sh
+++ dump_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -f .env ]; then
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ -f $DIR/.env ]; then
     while IFS='=' read -r key value; do
         [[ $key == \#* ]] && continue
 
@@ -10,12 +11,20 @@ if [ -f .env ]; then
         if [ -n "$key" ] && [ -n "$value" ]; then
             export "$key"="$value"
         fi
-    done < .env
+    done < $DIR/.env
 else
     echo "$0 - Error: .env file not found"
     exit 1
 fi
 
-docker exec -it $(docker ps -aqf ancestor=$PG_DB_SERVICE) sh -c "pg_dump -U $PG_DB_USER $PG_DB_NAME >> /tmp/$PG_DB_NAME.sql"
+vars=("PG_DB_SERVICE" "PG_DB_USER" "PG_DB_NAME")
+for var in "${vars[@]}"; do
+    if [ -z "${!var}" ]; then
+        echo "Error: $var is not set or has no value"
+        exit 1
+    fi
+done
 
-docker cp $(docker ps -aqf ancestor=$PG_DB_SERVICE):/tmp/$PG_DB_NAME.sql ./stonkinator_api/db_dumps/
+docker exec $(docker ps -aqf ancestor=$PG_DB_SERVICE) sh -c "pg_dump -U $PG_DB_USER $PG_DB_NAME >> /tmp/$PG_DB_NAME.sql"
+
+docker cp $(docker ps -aqf ancestor=$PG_DB_SERVICE):/tmp/$PG_DB_NAME.sql $DIR/stonkinator_api/db_dumps/
--- start.sh
+++ start.sh
@@ -4,9 +4,22 @@ cp .env ./stonkinator/stonkinator/trading_systems/env.py
 cp .env ./stonkinator/stonkinator/persistance/securities_db_py_dal/env.py
 cp .env ./stonkinator/.env
 cp .env ./stonkinator_api/.env
+cp .env ./alphascrape/.env
 
 if [[ $1 == "watch" ]]; then
     docker compose -f ./docker-compose.yml watch
 else
     docker compose -f ./docker-compose.yml up -d
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+[[ ! -d "$DIR/logs" ]] && mkdir "$DIR/logs"
+CRON_JOB="0 21 1 * * $DIR/dump_db.sh >> $DIR/logs/dump_db.log 2>&1"
+
+crontab -l | grep -q "$CRON_JOB"
+if [ $? -eq 0 ]; then
+    echo "Cron job already exists."
+else
+    (crontab -l; echo "$CRON_JOB") | crontab -
+    echo "Cron job added."
 fi
